c7292e1eda84caa195481ceb851e9f5cbba354d2
Nazar <nazarpas2007@gmail.com>
2026-07-08 21:59:54 +0300

    elec: add motor and motor_driver components

---
+++ electronics/components/motor.py
@@ -0,0 +1,22 @@
+from skidl import TEMPLATE, Part
+
+DC_Motor = Part(
+    "Motor",
+    "Motor_DC",
+    ref="M",
+    dest=TEMPLATE,
+    value="JGB37-545 (12V)",
+    # Part source
+    price_uah=550.0,
+    buy_link="https://prom.ua/ua/p2132897073-dvigatel-jgv37-545.html",
+    # Motor specs
+    torque_kg_cm=5,
+    no_load_rpm=330,
+    no_load_i_amp=0.2,
+    load_rpm=280,
+    load_i_amp=1,
+    stall_i_amp=3.8,
+    # Electrical specs
+    v_in_range=[6.0, 24.0],
+    i_max_in_amp=3.8,
+)
---
+++ electronics/components/motor_driver.py
@@ -0,0 +1,14 @@
+from skidl import TEMPLATE, Part
+
+Motor_Driver = Part(
+    "Bobot_Custom_Library",
+    "BTS7960_Module_IBT2M",
+    dest=TEMPLATE,
+    value="BTS7960 43A Driver Module",
+    # Part source
+    price_uah="193.5",
+    buy_link="https://prom.ua/ua/m-8558366577709891752-drajver-motorov-odnokanalnyj.html",
+    # Driver specs
+    i_limit_amp=43.0,
+    v_in_range=[6.0, 27.0],
+)
