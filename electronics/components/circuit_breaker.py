c6bca04db2448eeeffd7c8739ad5d6d51781d624
Nazar <nazarpas2007@gmail.com>
2026-08-02 18:18:06 +0300

    elec: add circuit breaker component

---
+++ electronics/components/circuit_breaker.py
@@ -0,0 +1,15 @@
+from skidl import TEMPLATE, Part
+
+Circuit_Breaker = Part(
+    "Device",
+    "CircuitBreaker_1P",
+    dest=TEMPLATE,
+    value="Circuit Breaker",
+    # Part source
+    price_uah=130.0,
+    buy_link="https://prom.ua/ua/p2666357477-razemy-amass-xt90s.html",
+    # Electrical specs
+    i_max_in_amp=90.0,
+    # Physical specs
+    weight_g=15
+)
